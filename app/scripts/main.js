660c187082fe84afd8b5038a6c19aebd4a6dfc93
Kyle Alwyn <kyle@lemonaid.com>
2016-04-29 16:30:54 -0700

    add ability to change id

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,57 @@
 $(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://itunes.apple.com/US/rss/customerreviews/id=962679660/sortBy=mostRecent/json',
-    dataType: 'json',
-    success: function (results) {
-      var reviews = results.feed.entry;
-      reviews.forEach((review) => {
+
+  let $hero = $('.hero'),
+      $input = $('#app-id-input'),
+      $submit = $('#app-id-submit'),
+      $appMeta = $('#app-meta'),
+      $appReviews = $('#app-reviews');
+
+  function getReviews(id, cb) {
+    $.get({
+      url: `https://itunes.apple.com/US/rss/customerreviews/id=${id}/sortBy=mostRecent/json`,
+      dataType: 'json',
+      success(results) {
+        cb(results);
+      },
+      error(err) {
+        cb(err);
+      }
+    });
+  }
+
+  $submit.on('click', event => {
+    event.preventDefault();
+    let input = $input.val();
+    if (!input) {
+      return;
+    }
+
+    $submit.addClass('loading');
+
+    getReviews(input, ({feed}) => {
+      $appReviews.empty();
+      console.log(feed);
+      feed.entry.forEach((review, index) => {
         if (!review.content) {
           return;
         }
 
-        var rating = '';
-        for (var i = 0; i < parseInt(review['im:rating'].label); i++) rating += '★';
+        let rating = '';
+        for (let i = 0; i < parseInt(review['im:rating'].label); i++) {
+          rating += '★';
+        }
 
-        var element = `
-          <div class="review">
-            <h1>${review.author.name.label}</h1>
+        $appReviews.append(`
+          <div id="review-${index}" class="review">
+            <h3 class="review-username">${review.author.name.label}</h3>
             <p>${review.content.label}</p>
             <span>${rating}</span>
           </div>
-        `;
+        `);
 
-        $('#reviews').append(element);
+        $hero.addClass('hero-shrunk');
+        $submit.removeClass('loading')
       })
-    }
-  });
+    })
+  })
 });
